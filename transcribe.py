2f8f2cc8b2696e0b79a1f99a8c8e1a3800fd7739
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2023-06-17 02:12:12 +0900

    Update transcribe.py to include whisper-cli's options
    Use as much as same options with whisper command which
    is executed as speech-recognition extensions of ELAN

--- transcribe.py
+++ transcribe.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import argparse
+import os
+from dataclasses import asdict
 import whisper
 
 
@@ -7,12 +9,11 @@ def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--model', type=str, default='base')
-    parser.add_argument('--input_file', type=str, default='input.mp3')
-    parser.add_argument('--output_file', type=str, default='output.txt')
+    parser.add_argument('--input_file', type=str, default='input.wav')
+    parser.add_argument('--output_format', type=str, default='tsv')
     parser.add_argument('--language', type=str, default='ja')
 
     args = parser.parse_args()
-
     return args
 
 
@@ -21,15 +22,31 @@ def main():
 
     model_name = args.model
     input_file = args.input_file
-    output_file = args.output_file
+    output_format = args.output_format
     language = args.language
 
+    output_dir = 'transcript'
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    writer = whisper.utils.get_writer(output_format, output_dir)
+
     whisper_model = whisper.load_model(model_name, download_root='./')
+    decode_options = whisper.DecodingOptions(
+        language=language,
+        best_of=5,
+        beam_size=5,
+        patience=1.0,
+    )
+    result = whisper_model.transcribe(
+        input_file,
+        verbose=False,
+        compression_ratio_threshold=2.4,
+        no_speech_threshold=0.6,
+        **asdict(decode_options)
+    )
 
-    result = whisper_model.transcribe(input_file, verbose=True, language=language)
-
-    with open(output_file, mode='w') as f:
-        f.write(result['text'])
+    writer(result, input_file)
 
 
 if __name__ == "__main__":
